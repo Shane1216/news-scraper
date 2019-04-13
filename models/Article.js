4b0d898dbb4316cac524fcd9511ddd65e9cff407
Shane <shanehill16@gmail.com>
2019-04-13 09:41:24 -0400

    Attempting to fix heroku

---
+++ models/Article.js
@@ -0,0 +1,34 @@
+// Require mongoose
+const mongoose = require("mongoose");
+// Create Schema class
+const Schema = mongoose.Schema;
+
+// Create article schema
+const ArticleSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  summary: {
+    type: String,
+//    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  saved: {
+    type: Boolean,
+    default: false
+  },
+  notes: [{
+     type: Schema.Types.ObjectId,
+     ref: "Note"
+  }]
+});
+
+// Create the Article model with the ArticleSchema
+const Article = mongoose.model("Article", ArticleSchema);
+
+// Export the model
+module.exports = Article;
